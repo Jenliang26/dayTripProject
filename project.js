9c677c63550bdfdb90e6dada1c6cba8a73c37580
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-07-06 16:11:06 -0500

    built a function to allow user to re-select
     Changes to be committed:
    	modified:   .DS_Store
    	modified:   project.js

--- project.js
+++ project.js
@@ -1,13 +1,13 @@
 'use strict';
 
-// let destinationArray = ["China", "Hong Kong", "France", "London", "Italy"];
-// let restaurantArray = ["MCD", "KFC", "Pizza Hut", "Wendy's", "Taco Bell"];
-// let transportationArray = ["Car", "Plane", "Boat", "Train", "Tesla"];
-// let entertainmentArray = ["Hiking", "Karaoke", "Volleyball", "Reading", "Sight seeing"]
+let destinationArray = ["China", "Hong Kong", "France", "London", "Italy"];
+let restaurantArray = ["MCD", "KFC", "Pizza Hut", "Wendy's", "Taco Bell"];
+let transportationArray = ["Car", "Plane", "Boat", "Train", "Tesla"];
+let entertainmentArray = ["Hiking", "Karaoke", "Volleyball", "Reading", "Sight seeing"]
 
-// let destinationArray = ["China", "Hong Kong", "France", "London", "Italy"];
-// let china = destinationArray[0];
-// console.log(destinationArray[0]);
+let destinationArray = ["China", "Hong Kong", "France", "London", "Italy"];
+let china = destinationArray[0];
+console.log(destinationArray[0]);
 
 let destinationArray = ["China", "Hong Kong", "France", "London", "Italy"];
 let randomNumber = Math.floor((Math.random () * destinationArray.length));
@@ -28,3 +28,35 @@ let entertainmentArray = ["Hiking", "Karaoke", "Volleyball", "Reading", "Sight s
 let randomNumber3 = Math.floor((Math.random () * entertainmentArray.length));
 console.log(randomNumber3);
 console.log(entertainmentArray[randomNumber3]);
+
+// not a function and missing the random part...
+let destinationArray = ["China", "Hong Kong", "France", "London", "Italy"];
+let randomNumber = Math.floor((Math.random () * destinationArray.length));
+for(let i = 0; i < destinationArray.length; i++){
+    if(destinationArray[i] === "Hong Kong"){
+        console.log("Let's go!");
+    }
+    else {
+        console.log("Don't want to go there.");
+    }
+}
+
+// tried with the below function. Could not get it right. 
+let topChoice = "Hong Kong";
+// let location = getLocationPreference(topChoice);
+
+function getLocationPreference(topChoice){
+    if(topChoice === "Hong Kong"){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+console.log(getLocationPreference);
+
+// Know I need to be able to display the random choices the user is satisfied with. But how? Would need to make above functions work first. 
+// Once that is complete, then I would console.log my choices. 
+
+
+console.log("We will be going to " + destination + " eating " + restaurant + " by " + transportation + " and will be doing " + entertainment + " after ");
